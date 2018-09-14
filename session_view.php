87c860412e8f08716896f26bded468d4eebf1630
Garvit Khurana <garvit.khurana@gmail.com>
2018-09-14 17:36:10 +0530

    sessions added

--- session_view.php
+++ session_view.php
@@ -2,8 +2,13 @@
 session_start();
 
 // Echo session variables that were set on previous page
-echo "Favorite color is " . $_SESSION["favcolor"] . ".<br>";
-echo "Favorite animal is " . $_SESSION["favanimal"] . ".<br>";
+
+$c=$_SESSION["favcolor"];
+$a=$_SESSION["favanimal"];
+echo "Favorite color is " . $c. ".<br>";
+echo "Favorite animal is " . $a . ".<br>";
+
+// $_SESSION["favanimal"]="dog";
 
 print_r($_SESSION);
 ?>
