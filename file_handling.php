55cd224b60d176fd3844457b9c6ee07c46622bff
Garvit Khurana <garvit.khurana@gmail.com>
2018-09-15 10:53:17 +0530

    fh using fread

---
+++ file_handling.php
@@ -0,0 +1,7 @@
+
+// Reading a file using fread
+<?php
+$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+echo fread($myfile,filesize("webdictionary.txt"));
+fclose($myfile);
+?>
