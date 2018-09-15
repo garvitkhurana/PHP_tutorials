c62dd5cde3e12156bcf3aa6a50685426e1e48b83
Garvit Khurana <garvit.khurana@gmail.com>
2018-09-15 10:55:07 +0530

    fgets updated

--- file_handling.php
+++ file_handling.php
@@ -4,4 +4,20 @@
 $myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
 echo fread($myfile,filesize("webdictionary.txt"));
 fclose($myfile);
+
+echo "<br>----------------------------------------------<br>";
+
+// Reading a file line using fgets
+$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+echo fgets($myfile);
+fclose($myfile);
+
+
+// Reading a file using fgets
+$myfile = fopen("webdictionary.txt", "r") or die("Unable to open file!");
+// Output one line until end-of-file
+while(!feof($myfile)) {
+  echo fgets($myfile) . "<br>";
+}
+fclose($myfile);
 ?>
