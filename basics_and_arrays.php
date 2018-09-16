67c4d4d5f5e81bf143bbd65d58bb5529f56fd4a4
Garvit Khurana <garvit.khurana@gmail.com>
2018-09-16 14:35:32 +0530

    file updated

--- basics_and_arrays.php
+++ basics_and_arrays.php
@@ -72,4 +72,17 @@ echo $a[2][0],"<br>";
 echo $a[2][1],"<br>";
 echo $a[2][2],"<br>";
 
+
+
+$age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43","Harry"=>"50");
+print_r(array_chunk($age,2,true));
+
+// print_r(array_chunk($age,2));
+
+
+
+
+
+
+
 ?>
