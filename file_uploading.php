b45306ce21ffcd1a622383a090499842af922a2a
Garvit Khurana <garvit.khurana@gmail.com>
2018-09-17 14:31:59 +0530

    file uploading

--- file_uploading.php
+++ file_uploading.php
@@ -10,3 +10,21 @@
 
 </body>
 </html>
+
+
+// For uploading text file
+
+
+<!-- <!DOCTYPE html>
+<html>
+<head>
+  <title>Upload your files</title>
+</head>
+<body>
+  <form enctype="multipart/form-data" action="upload.php" method="POST">
+    <p>Upload your file</p>
+    <input type="file" name="uploaded_file"></input><br />
+    <input type="submit" value="Upload"></input>
+  </form>
+</body>
+</html> -->
