aed582352d9fb94aca8edc37288b0c73c51e513c
Garvit Khurana <garvit.khurana@gmail.com>
2018-09-18 15:47:24 +0530

    completed

---
+++ sql_operations.php
@@ -0,0 +1,56 @@
+<?php
+require("mysql_connect.php");
+
+// if (mysqli_query($link,"CREATE DATABASE sample"))
+//  	  echo "Database created<br>";
+// else
+//   echo "Error creating database: " . mysqli_error($link);
+//
+//
+$db_connect=mysqli_select_db($link,"IMDB");
+
+
+echo "<br>----------------------------------------------<br>";
+
+
+// $query="show databases";
+// $result=mysqli_query($link,$query);
+// if($result)
+// echo "Query has been executed<br>";
+// while ($ans=mysqli_fetch_assoc($result)) {
+//   foreach ($ans as $key) {
+//     echo "$key<br>";
+//   }
+// }
+
+
+echo "<br>----------------------------------------------<br>";
+
+
+//
+$query="select name from movies where name like'%i'";
+$result=mysqli_query($link,$query);
+if($result)
+{echo "Query has been executed<br>";}
+else
+{echo "error is: ". mysqli_error($link);}
+while ($ans=mysqli_fetch_assoc($result))
+{
+  foreach ($ans as $key) {
+    echo "$key<br>";
+  }
+}
+
+
+echo "<br>----------------------------------------------<br>";
+
+
+
+
+
+mysqli_close($link);
+
+
+
+
+?>
